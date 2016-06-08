25e285bf332d26bfd23278b7a33f9613f10d2e3a
Ivan Kalita <input.txt@hotmail.com>
2016-06-08 10:23:12 +1000

    ECP-518: Добавил тип - пол пользователя

---
+++ src/Type/GenderType.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ivan Kalita
+ * Date: 08.06.16
+ * Time: 10:16.
+ */
+namespace MayakRed\ECheepIntegration\Type;
+
+class GenderType
+{
+    const MALE = 'male';
+    const FEMALE = 'female';
+}
