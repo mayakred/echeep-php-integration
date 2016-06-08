d4afc039f35e9c633edd5dca1e3c5ac3a06ffc7b
Ivan Kalita <input.txt@hotmail.com>
2016-06-08 10:36:16 +1000

    ECP-518: Добавил тип единиц измерения специального предложения

---
+++ src/Type/UnitType.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ivan Kalita
+ * Date: 08.06.16
+ * Time: 10:34.
+ */
+namespace Type;
+
+class UnitType
+{
+    const CURRENCY = 'currency';
+    const PERCENT = 'percent';
+    const CUSTOM = 'custom';
+}
