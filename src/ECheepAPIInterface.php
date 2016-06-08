2cc8d915f875b024a433a73438c7432abc806acb
Ivan Kalita <input.txt@hotmail.com>
2016-06-08 11:03:52 +1000

    ECP-518: Добавил интерфейс методов API

---
+++ src/ECheepAPIInterface.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace MayakRed\ECheepIntegration;
+
+use MayakRed\ECheepIntegration\Model\Promotion;
+use MayakRed\ECheepIntegration\Model\User;
+use MayakRed\ECheepIntegration\Model\UserPromotion;
+use MayakRed\ECheepIntegration\Request\Sale;
+use MayakRed\ECheepIntegration\Request\UserPromotionIssuance;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ivan Kalita
+ * Date: 08.06.16
+ * Time: 9:40.
+ */
+interface ECheepAPIInterface
+{
+    /**
+     * @param $phone
+     *
+     * @return bool
+     */
+    public function getUserByPhoneRequest($phone);
+
+    /**
+     * @param $phone
+     * @param $code
+     *
+     * @return User
+     */
+    public function getUserByPhoneConfirm($phone, $code);
+
+    /**
+     * @param $uuid
+     *
+     * @return User
+     */
+    public function getUserByChipUUID($uuid);
+
+    /**
+     * @param Sale $sale
+     *
+     * @return bool
+     */
+    public function registerSale(Sale $sale);
+
+    /**
+     * @param UserPromotionIssuance $issuance
+     *
+     * @return UserPromotion
+     */
+    public function createUserPromotion(UserPromotionIssuance $issuance);
+
+    /**
+     * @return Promotion[]
+     */
+    public function getOrganizationPromotions();
+}
