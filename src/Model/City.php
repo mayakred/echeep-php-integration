7ba3a5914df1a88c3b57516953576e4d1e4754ee
Ivan Kalita <input.txt@hotmail.com>
2016-06-08 10:23:25 +1000

    ECP-518: Добавил модели пользователя и города

---
+++ src/Model/City.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ivan Kalita
+ * Date: 08.06.16
+ * Time: 10:18.
+ */
+namespace MayakRed\ECheepIntegration\Model;
+
+class City
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var float
+     */
+    protected $lat;
+
+    /**
+     * @var float
+     */
+    protected $lon;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLat()
+    {
+        return $this->lat;
+    }
+
+    /**
+     * @param float $lat
+     *
+     * @return $this
+     */
+    public function setLat($lat)
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLon()
+    {
+        return $this->lon;
+    }
+
+    /**
+     * @param float $lon
+     *
+     * @return $this
+     */
+    public function setLon($lon)
+    {
+        $this->lon = $lon;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
---
+++ src/Model/User.php
@@ -0,0 +1,162 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ivan Kalita
+ * Date: 08.06.16
+ * Time: 10:15.
+ */
+namespace MayakRed\ECheepIntegration\Model;
+
+class User
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $firstName;
+
+    /**
+     * @var string
+     */
+    protected $middleName;
+
+    /**
+     * @var string
+     */
+    protected $lastName;
+
+    /**
+     * @var string
+     */
+    protected $gender;
+
+    /**
+     * @var bool
+     */
+    protected $isConfirmed;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     *
+     * @return $this
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMiddleName()
+    {
+        return $this->middleName;
+    }
+
+    /**
+     * @param string $middleName
+     *
+     * @return $this
+     */
+    public function setMiddleName($middleName)
+    {
+        $this->middleName = $middleName;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     *
+     * @return $this
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param string $gender
+     *
+     * @return $this
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isIsConfirmed()
+    {
+        return $this->isConfirmed;
+    }
+
+    /**
+     * @param bool $isConfirmed
+     *
+     * @return $this
+     */
+    public function setIsConfirmed($isConfirmed)
+    {
+        $this->isConfirmed = $isConfirmed;
+
+        return $this;
+    }
+}
